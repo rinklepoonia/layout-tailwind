8fa8c703d5d9c45a1e404f0305218f337bbccbdb
rinklepoonia <rinklepoonia889@gmail.com>
2024-01-13 09:41:02 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+       'Work-Sans' :["Work-Sans", "sarif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
